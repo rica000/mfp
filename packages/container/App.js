335ef81e9082cd7e0cf0ea94e40756e14f3327d0
João Carvalho <guerra@winsocial.com.br>
2023-10-11 17:58:15 -0300

    added header to container

--- packages/container/App.js
+++ packages/container/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+
 export default () => {
   console.log("trigger container workflow");
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
